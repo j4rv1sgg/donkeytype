f9af56b6e9803c61cb8ca0fbbc2873a23c90607b
j4rv1sgg <jrvs.inc@gmail.com>
2024-08-29 16:13:55 +0200

    add color preview

--- frontend/src/components/Header/ThemePopup.tsx
+++ frontend/src/components/Header/ThemePopup.tsx
@@ -3,11 +3,12 @@
 import { useContext, useState } from 'react';
 import Popup from '@/components/PopUp';
 import { Palette } from 'lucide-react';
-import styles from './ThemePopup.module.css'
+import styles from './ThemePopup.module.css';
 import { ThemeContext } from '@/context/ThemeContext';
 
 const themes = [
-  {title: 'retro',
+  {
+    title: 'retro',
     data: {
       '--bg-color': '#dad3c1',
       '--main-color': '#1d1b17',
@@ -17,7 +18,7 @@ const themes = [
       '--text-color': '#1d1b17',
       '--error-color': '#bf616a',
       '--error-extra-color': '#793e44',
-    }
+    },
   },
   {
     title: 'ryujinscalse',
@@ -29,8 +30,8 @@ const themes = [
       '--sub-alt-color': '#040e1d',
       '--text-color': '#f6f0e9',
       '--error-color': '#9b333a',
-      '--error-extra-color': '#7e2a33'
-    }
+      '--error-extra-color': '#7e2a33',
+    },
   },
   {
     title: 'bushido',
@@ -42,40 +43,72 @@ const themes = [
       '--sub-alt-color': '#040e1d',
       '--text-color': '#ffe4bc',
       '--error-color': '#ca4754',
-      '--error-extra-color': '#7e2a33'
-    }
+      '--error-extra-color': '#7e2a33',
+    },
   },
-
-]
+  {
+    title: 'arch',
+    data: {
+      '--bg-color': '#0c0d11',
+      '--main-color': '#7ebab5',
+      '--caret-color': '#7ebab5',
+      '--sub-color': '#454864',
+      '--sub-alt-color': '#171a25',
+      '--text-color': '#f6f5f5',
+      '--error-color': '#ff4754',
+      '--error-extra-color': '#b02a33',
+    },
+  },
+  {
+    title: 'art hero',
+    data: {
+      '--bg-color': '#00002e',
+      '--main-color': '#ffadad',
+      '--caret-color': '#ffffff',
+      '--sub-color': '#ff3d8b',
+      '--sub-alt-color': '#060548',
+      '--text-color': '#f1deef',
+      '--error-color': '#8fecff',
+      '--error-extra-color': '#558cab',
+    },
+  },
+];
 
 export default function ThemePopup() {
   const [isOpen, setIsOpen] = useState(false);
-  const [theme, updateTheme] = useContext(ThemeContext)
+  const [theme, updateTheme] = useContext(ThemeContext);
   return (
     <>
-        <Palette
+      <Palette
+        className={styles.icon}
+        size={23}
         onClick={() => {
           setIsOpen(true);
         }}
-        />
+      />
       <Popup show={isOpen} darkBg={true} onClose={() => setIsOpen(false)}>
         <h2>Theme</h2>
-          <div className={styles.list}>
-            {themes.map((item, i) => {
-              return (
-                <li
-                  key={i}
-                  className={theme.title == item.title ? styles.active: ''}
-                  onClick={() => {
-                    updateTheme(item)
-                    setIsOpen(false);
-                  }}
-                >
-                  {item.title}
-                </li>
-              );
-            })}
-          </div>
+        <div className={styles.list}>
+          {themes.map((item, i) => {
+            return (
+              <li
+                key={i}
+                className={theme.title == item.title ? styles.active : ''}
+                onClick={() => {
+                  updateTheme(item);
+                  setIsOpen(false);
+                }}
+              >
+                <span>{item.title}</span>
+                <div style={{'backgroundColor': item.data['--bg-color']}} className={styles.colorPrevContainer}>
+                  <div style={{'backgroundColor': item.data['--main-color']}} className={styles.colorPrevItem}></div>
+                  <div style={{'backgroundColor': item.data['--sub-color']}} className={styles.colorPrevItem}></div>
+                  <div style={{'backgroundColor': item.data['--text-color']}} className={styles.colorPrevItem}></div>
+                </div>
+              </li>
+            );
+          })}
+        </div>
       </Popup>
     </>
   );
